af37fa56a9bc967a4f87fc29cf3c43060e292603
2191706683 <2191706683@qq.com>
2023-05-31 01:19:09 +0800

    搜索界面

--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -1,33 +1,34 @@
 <template>
-    <div>
-        <Navbar />
-        <form action="/">
-        <van-search
-            class="search"
-            v-model="value"
-            show-action
-            autofocus
-            clearable
-            placeholder="请输入搜索关键词"
-            @search="onSearch"
-            @cancel="onCancel"
-            @click-left-icon="onSearch"
-        />
-        </form>
-    </div>
+  <!--  -->
+  <div>
+    <Navbar />
+    <form action="/">
+      <van-search
+        class="search"
+        v-model="value"
+        show-action
+        autofocus
+        clearable
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="onCancel"
+        @click-left-icon="onSearch"
+      />
+    </form>
+  </div>
 </template>
 
 <script setup>
-import Navbar from '@/components/Navbar.vue'
-import { ref } from 'vue';
-import { showToast } from 'vant'
-import _ from 'lodash'
+import Navbar from "@/components/Navbar.vue";
+import { ref } from "vue";
+import { showToast } from "vant";
+import _ from "lodash";
 
-const value = ref(null)
+const value = ref(null);
 
 const ajax1 = () => {
-    console.log('开始搜索，搜索内容为' + value.value)
-}
+  console.log("开始搜索，搜索内容为" + value.value);
+};
 
 // const ajax2 = () => {
 //     if (!value.value) {
@@ -37,25 +38,22 @@ const ajax1 = () => {
 //     }
 // }
 
-let debounceAjax1 = _.debounce(ajax1, 1000)
+let debounceAjax1 = _.debounce(ajax1, 1000);
 // let debounceAjax2 = _.debounce(ajax2, 1000)
 
 const onSearch = () => {
-    if (!value.value) {
-        showToast('搜索内容为空，请输入内容')
-        return
-    }
-    debounceAjax1()
-}
+  if (!value.value) {
+    showToast("搜索内容为空，请输入内容");
+    return;
+  }
+  debounceAjax1();
+};
 
 // const onUpdate = () => {
 //     debounceAjax2()
 // }
 
-
-
-const onCancel = () => history.back()
-
+const onCancel = () => history.back();
 </script>
 
 <style lang="stylus" scoped>
