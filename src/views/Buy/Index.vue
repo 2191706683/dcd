54ffa769ee194b991e0f73fca1a43d4cf4c041f2
2191706683 <2191706683@qq.com>
2023-05-31 01:35:24 +0800

    购买车页面

--- src/views/Buy/Index.vue
+++ src/views/Buy/Index.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="buy">
-        <div class="tip">提交后会有经销商与您联系</div>
-        <Navbar :title="buyHeader.name" />
-        <BuyHeader :buyHeader="buyHeader" />
-        <BuyForm />
-    </div>
+  <!--  -->
+  <div class="buy">
+    <div class="tip">提交后会有经销商与您联系</div>
+    <Navbar :title="buyHeader.name" />
+    <BuyHeader :buyHeader="buyHeader" />
+    <BuyForm />
+  </div>
 </template>
-  
+
 <script setup>
 // 引入页面组件
-import Navbar from '@/components/Navbar.vue';
-import BuyHeader from './BuyHeader.vue'
-import BuyForm from './BuyForm.vue';
+import Navbar from "@/components/Navbar.vue";
+import BuyHeader from "./BuyHeader.vue";
+import BuyForm from "./BuyForm.vue";
 
-import {computed } from 'vue'
-import { useRoute } from 'vue-router';
+import { computed } from "vue";
+import { useRoute } from "vue-router";
 
-const route = useRoute()
+const route = useRoute();
 
 // 对跳转到该页面传过来的参数进行定义，该参数为单个车的数据或detail中的头部数据
-const buyHeader = computed(() => route.query)
-
+const buyHeader = computed(() => route.query);
 </script>
 
 <style lang="stylus" scoped>
