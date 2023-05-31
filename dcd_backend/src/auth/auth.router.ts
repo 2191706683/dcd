2b41adebf96825cfc8970508c454dc4eca3db1b5
2191706683 <2191706683@qq.com>
2023-05-31 15:13:42 +0800

    配置验证登录的路径

--- dcd_backend/src/auth/auth.router.ts
+++ dcd_backend/src/auth/auth.router.ts
@@ -3,7 +3,7 @@ const routers = require('koa-router')();
 import { authGuard } from './auth.middleware';
 
 /**
- * 验证登录
+ * /验证登录
  */
 routers.post('/auth/validate', authGuard, authController.validate);
 
